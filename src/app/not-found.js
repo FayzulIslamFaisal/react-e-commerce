fa405de5ada0cb6886d337f4488f27f1f118ec43
FayzulIslamFaisal <mfbfaisal@gmail.com>
2024-10-11 15:13:43 +0600

    not found page redesign

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const notFound = () => {
   return (
-    <div className="text-whit ">
-      <div>
+    <div className="text-white h-screen flex items-center justify-center container mx-auto px-4 ">
+      <div className="text-white">
         <h2>Not Found</h2>
         <p>Could not find requested resource</p>
         <Link href="/">Return Home</Link>
